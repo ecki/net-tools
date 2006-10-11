2af58905557e26002b4df9f205d49ae0ba6f3a46
Bernd Eckenfels <net-tools@lina.inka.de>
2006-10-11 01:14:33 +0000

    fixed signedness compiler warning

--- include/util-ank.h
+++ include/util-ank.h
@@ -75,6 +75,6 @@ extern int do_qdisc(int argc, char **argv);
 extern int do_class(int argc, char **argv);
 extern int do_filter(int argc, char **argv);
 
-extern const char *format_host(int af, void *addr, __u8 *abuf, int alen);
+extern const char *format_host(int af, void *addr, char *abuf, int alen);
 
 #endif /* __UTILS_H__ */
--- lib/util-ank.c
+++ lib/util-ank.c
@@ -293,7 +293,7 @@ int inet_addr_match(inet_prefix *a, inet_prefix *b, int bits)
 	return 0;
 }
 
-const char *format_host(int af, void *addr, __u8 *abuf, int alen)
+const char *format_host(int af, void *addr, char *abuf, int alen)
 {
 #ifdef RESOLVE_HOSTNAMES
 	if (resolve_hosts) {
