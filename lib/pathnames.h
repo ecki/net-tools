2246c6430a476889d7782bc11046966bcfabe723
Lubomir Rintel <lkundrak@v3.sk>
2014-10-03 10:49:07 +0200

    pathnames: Bluetooth stuff is now in debugfs
    Yay for the ABI breakage.

--- lib/pathnames.h
+++ lib/pathnames.h
@@ -53,8 +53,8 @@
 #define _PATH_PROCNET_X25_ROUTE		"/proc/net/x25/route"
 #define _PATH_PROCNET_DEV_MCAST		"/proc/net/dev_mcast"
 #define _PATH_PROCNET_ATALK_ROUTE	"/proc/net/atalk_route"
-#define _PATH_SYS_BLUETOOTH_L2CAP	"/sys/class/bluetooth/l2cap"
-#define _PATH_SYS_BLUETOOTH_RFCOMM	"/sys/class/bluetooth/rfcomm"
+#define _PATH_SYS_BLUETOOTH_L2CAP	"/sys/kernel/debug/bluetooth/l2cap"
+#define _PATH_SYS_BLUETOOTH_RFCOMM	"/sys/kernel/debug/bluetooth/rfcomm"
 
 /* pathname for the netlink device */
 #define _PATH_DEV_ROUTE	"/dev/route"
